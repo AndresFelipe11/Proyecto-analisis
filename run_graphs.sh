908347256b462964e0db2eafcc8db0c7be15adaa
Juan José González Giraldo <juan.1701716389@ucaldas.edu.co>
2021-12-13 22:15:41 -0500

    Undo all partition config

--- run_graphs.sh
+++ run_graphs.sh
@@ -3,9 +3,7 @@
 export PYPHI_WELCOME_OFF='yes'
 
 for graph in $(ls executionFiles); do
-  if [[ $graph == *"_bi_"* ]]; then
-    echo "----------" >>results_sh.txt
-    echo $graph >>results_sh.txt
-    python executionFiles/$graph >>results_sh.txt
-  fi
+  echo "----------" >>results_sh.txt
+  echo $graph >>results_sh.txt
+  python executionFiles/$graph >>results_sh.txt
 done
